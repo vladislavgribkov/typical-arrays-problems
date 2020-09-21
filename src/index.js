5b9edbbff4b6413f53d5378365ed70835cdb9928
vladislavgribkov <vladislavgrbkovq@yandex.by>
2020-09-22 01:02:22 +0300

    feat: complited task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
+    if(array !==undefined){
+        if(array.length !==0){
+            return Math.min(...array);
+        }
+    }
   return 0;
 }
 
 exports.max = function max (array) {
+    if(array !==undefined){
+        if(array.length !==0){
+            return Math.max(...array);
+        }
+    }
   return 0;
 }
 
 exports.avg = function avg (array) {
+    if(array !==undefined){
+        if(array.length !==0){
+        let count = array.reduce(function(sum, element) {
+            return sum + element;
+          }, 0);
+          return count / array.length;
+        }
+    }
   return 0;
 }
